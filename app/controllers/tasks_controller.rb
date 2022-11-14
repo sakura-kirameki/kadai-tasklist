accb59ad7f3b5bb671ea931401aae821a774d44d
sakura.kirameki <sakura.kirameki.x@gmail.com>
2022-11-14 01:17:26 +0000

    error fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+     @task = current_user.tasks.build #form_with
+     @tasks = current_user.tasks
+    end
   end
 
   def show
@@ -13,11 +17,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
@@ -30,7 +34,7 @@ class TasksController < ApplicationController
   def update
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to @tasks
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
@@ -46,7 +50,7 @@ class TasksController < ApplicationController
   
   private
 
-  def set_task
+  def set_tasks
     @task = Task.find(params[:id])
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   
   def index
+   @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
