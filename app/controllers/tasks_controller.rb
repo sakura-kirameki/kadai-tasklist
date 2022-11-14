3d4b528602c38e1207f8e33dc26fa65ec4d2f953
sakura.kirameki <sakura.kirameki.x@gmail.com>
2022-11-14 11:09:21 +0000

    error fix - edit part

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
@@ -50,14 +50,22 @@ class TasksController < ApplicationController
   
   private
 
-  def set_tasks
-    @task = Task.find(params[:id])
-  end
+  #def set_task
+  #  @task = Task.find(params[:id])
+  #end
   
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+    
   
 end
 
